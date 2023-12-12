be41d31d1c602f6fab7cf228be31ff7d6dde29c8
asliddin727 <asliddinmeyliyev1@gmail.com>
2023-12-12 11:50:50 +0500

    upload files

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict'
+
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+// function min(a, b){
+//     a > b ? console.log(b) : console.log(a)
+// }
+
+// min(0, -1)
+
+// ================================================================================================
+
+
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+// function pow(a, n){
+//     a = a**n
+//     console.log(a)
+// }
+
+// pow(3, 3)
+
+// ================================================================================================
+
+// ALGORITHM
+// let salaries = {
+//     Ann:200,
+//     John:400,
+//     Albert:500,
+//     Jessica:"100",
+// }
+
+// let total = 0
+
+// for (let key in salaries) {
+//     if(typeof salaries[key] === 'number'){
+//         total += salaries[key];
+//     }
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// for (let key in salaries) {
+//     if(!isNaN(salaries[key])){
+//         total += Number(salaries[key]);
+//     }
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(`Jami narxi: ${total}`)
